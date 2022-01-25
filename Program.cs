956d98bfb901eef30658457980f6227ea12ef634
Farook M Fazrin <nirzaf1@hotmail.com>
2022-01-25 20:40:36 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿
+Vehicle v1 = new Vehicle();
+
+v1.Drive();
+v1.Brake();
+v1.Park();
+
+//Crate a new class for Vehicle with Interface implementation of Drive, Brake, and Park
+public class Vehicle : IDrive, IBrake, IPark
+{
+    public void Drive()
+    {
+        Console.WriteLine("Vehicle is driving");
+    }
+
+    public void Brake()
+    {
+        Console.WriteLine("Vehicle is braking");
+    }
+
+    public void Park()
+    {
+        Console.WriteLine("Vehicle is parking");
+    }
+}
+
+public interface IDrive
+{
+    void Drive();
+}
+
+public interface IBrake
+{
+    void Brake();
+}
+
+public interface IPark
+{
+    void Park();
+}
+
+
+
